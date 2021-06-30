35613903a50582def310f73f5a1200c2b89ec141
khansaifff <2khan.saif@gmail.com>
2021-06-30 14:57:28 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
-import "./style.css";
+/**
+ * Main APP
+ */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+// Firebase
+import './firebase';
+
+// Hulk CSS
+import './lib/hulkCss';
+// App Conatiner
+import App from './container/App';
+
+// Store
+import { store } from './store';
+
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
+function MainApp() {
+	return (
+      <Provider store={store}>
+			<MuiPickersUtilsProvider utils={MomentUtils}>
+				<Router>
+					<Switch>
+						<Route path="/" component={App} />
+					</Switch>
+				</Router>
+			</MuiPickersUtilsProvider>
+      </Provider>
+	);
 }
+
+export default MainApp;
